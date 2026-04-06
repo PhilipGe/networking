d38fe43340418daceecaa3d7052397f94a99b23e
PhilipGe <philip.gessen@gmail.com>
2026-04-06 14:04:41 -0400

    Abstracted ssh authentication pipeline. Added augmented command that open local and remote ports

--- translators/ssh_utils.py
+++ translators/ssh_utils.py
@@ -4,15 +4,21 @@ import os
 
 from entities.interface import Interface
 from entities.port import Socket
+from entities.tunnel import Tunnel
 
-def open_ssh_shell(ssh_socket: Socket, username: str, password: str):
-    pid, fd = pty.fork()
-
-    if pid == 0:
-        os.execvp("ssh", ["ssh",f"{username}@{ssh_socket.ip}", "-p", f"{ssh_socket.port}"])
-    else:
-        buffer = ''
-        while not (('password' in buffer) or ('(yes/no)' in buffer)):
+def _ssh_authenticate(fd, password):
+    buffer = ''
+    while not (('password' in buffer) or ('(yes/no)' in buffer)):
+        try:
+            data = os.read(fd, 1024)
+            buffer += data.decode()
+            print(data.decode())
+        except OSError:
+            break
+    
+    if('(yes/no)' in buffer):
+        os.write(fd, ('yes\n').encode())
+        while not ('password' in buffer):
             try:
                 data = os.read(fd, 1024)
                 buffer += data.decode()
@@ -20,23 +26,47 @@ def open_ssh_shell(ssh_socket: Socket, username: str, password: str):
             except OSError:
                 break
         
-        if('(yes/no)' in buffer):
-            os.write(fd, ('yes\n').encode())
-            while not ('password' in buffer):
-                try:
-                    data = os.read(fd, 1024)
-                    buffer += data.decode()
-                    print(data.decode())
-                except OSError:
-                    break
-            
-        if('password' in buffer):
-            os.write(fd, (password+'\n').encode())
+    if('password' in buffer):
+        os.write(fd, (password+'\n').encode())
 
-        while True:
-            try:
-                data = os.read(fd, 1024)
-                if(not data): break
-                print(data.decode())
-            except OSError:
-                break
+    while True:
+        try:
+            data = os.read(fd, 1024)
+            if(not data): break
+            print(data.decode())
+        except OSError:
+            break
+
+def open_ssh_shell(ssh_socket: Socket, username: str, password: str):
+    pid, fd = pty.fork()
+
+    if pid == 0:
+        os.execvp("ssh", ["ssh",f"{username}@{ssh_socket.ip}", "-p", f"{ssh_socket.port}"])
+    else:
+        _ssh_authenticate(fd, password)
+
+def open_ssh_local_tunnel(tunnel: Tunnel, username: str, password: str):
+    pid, fd = pty.fork()
+
+    if pid == 0:
+        os.execvp("ssh", [
+            "ssh",f"{username}@{tunnel.forwarder_socket.ip}", "-p", f"{tunnel.forwarder_socket.port}",
+            "-L",
+            f"{tunnel.creator_socket.port}:{tunnel.target_socket.ip}:{tunnel.target_socket.port}",
+            "-NT"
+        ])
+    else:
+        _ssh_authenticate(fd, password)
+
+def open_ssh_remote_tunnel(tunnel: Tunnel, username: str, password: str):
+    pid, fd = pty.fork()
+
+    if pid == 0:
+        os.execvp("ssh", [
+            "ssh",f"{username}@{tunnel.creator_socket.ip}", "-p", f"{tunnel.creator_socket.port}",
+            "-R",
+            f"{tunnel.forwarder_socket.port}:{tunnel.target_socket.ip}:{tunnel.target_socket.port}",
+            "-NT"
+        ])
+    else:
+        _ssh_authenticate(fd, password)
