3c4e97c3b136b513a72fa427c5a5aacb0f45697b
PhilipGe <philip.gessen@gmail.com>
2026-04-06 13:56:51 -0400

    Fixed encoding and api misuse issues

--- translators/ssh_utils.py
+++ translators/ssh_utils.py
@@ -9,27 +9,29 @@ def open_ssh_shell(ssh_socket: Socket, username: str, password: str):
     pid, fd = pty.fork()
 
     if pid == 0:
-        os.execvp("ssh", ["ssh",f"{username}@{ssh_socket.ip} -p {ssh_socket.port}"])
+        os.execvp("ssh", ["ssh",f"{username}@{ssh_socket.ip}", "-p", f"{ssh_socket.port}"])
     else:
         buffer = ''
-        while not (('password' in buffer) and ('(yes/no)' not in buffer)):
+        while not (('password' in buffer) or ('(yes/no)' in buffer)):
             try:
                 data = os.read(fd, 1024)
                 buffer += data.decode()
+                print(data.decode())
             except OSError:
                 break
         
         if('(yes/no)' in buffer):
-            os.write(fd, ('yes\n').decode())
+            os.write(fd, ('yes\n').encode())
             while not ('password' in buffer):
                 try:
                     data = os.read(fd, 1024)
                     buffer += data.decode()
+                    print(data.decode())
                 except OSError:
                     break
             
         if('password' in buffer):
-            os.write(fd, (password+'\n').decode())
+            os.write(fd, (password+'\n').encode())
 
         while True:
             try:
