cf860f554ef32839886c90f20f81164894bcf1f4
PhilipGe <philip.gessen@gmail.com>
2026-04-06 13:31:43 -0400

    Added main/py with a test case

---
+++ main.py
@@ -0,0 +1,10 @@
+from entities.service_enum import Service
+from translators.ssh_utils import *
+
+if __name__ == '__main__':
+    
+    USERNAME = 'net1_student5'
+    PASSWORD = 'password5'
+
+    sock = Socket(None, '10.50.158.236', 7777, Service.SSH)
+    open_ssh_shell(sock, USERNAME, PASSWORD)
